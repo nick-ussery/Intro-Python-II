f214f946d539f8b5a799efc76d60e8a190edb71b
nick-ussery <nick.ussery@gmail.com>
2020-08-06 11:34:31 -0500

    mvp

--- src/room.py
+++ src/room.py
@@ -1,2 +1,15 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room():
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+    n_to = ''
+    s_to = ''
+    e_to = ''
+    w_to = ''
+#
+
+    def __str__(self):
+        return ("name: {} description: {}".format(self.name, self.description))
